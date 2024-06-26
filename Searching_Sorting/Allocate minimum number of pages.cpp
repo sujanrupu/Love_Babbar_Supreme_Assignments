7811fe6b1e831fb60ffb5fc419404a245500b162
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-06-26 22:54:48 +0530

    Create Allocate minimum number of pages.cpp

---
+++ Searching_Sorting/Allocate minimum number of pages.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    bool check(int arr[], int n, int m, int ans) {
+        int count = 1, totalPage = 0;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] > ans)    return false;
+            else if(totalPage + arr[i] > ans) {
+                count++;
+                totalPage = arr[i];
+                if(count > m)   return false;
+            }
+            else    totalPage += arr[i];
+        }
+        return true;
+    }
+    
+    long long findPages(int n, int arr[], int m) {
+        if(m > n)   return -1;
+        long long start = 0, end = 0, ans = -1;
+        for(int i = 0; i < n; i++)
+            end += arr[i];
+        while(start <= end) {
+            long long mid = start + (end - start)/2;
+            if(check(arr, n, m, mid)) {
+                ans = mid;
+                end = mid - 1;
+            }
+            else    start = mid + 1;
+        }
+        return ans;
+    }
+};
