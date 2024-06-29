86fb81dcfd9dd79a7ca884ead07d82fbab5eb2f9
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-06-29 21:23:20 +0530

    Create Best Time to Buy and Sell Stock IV.cpp

---
+++ Dynamic Programming/Best Time to Buy and Sell Stock IV.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(vector<int>& prices, int index, int buy, int limit, vector<vector<vector<int>>>& dp) {
+        if(index >= prices.size() || limit == 0)  return 0;
+        if(dp[index][buy][limit]!=-1)  return dp[index][buy][limit];
+        int profit = 0;
+        if(buy) {
+            int buy = -prices[index] + solve(prices, index+1, 0,limit, dp);
+            int skip = 0 + solve(prices, index+1, 1, limit, dp);
+            profit = max(buy, skip);
+        }
+        else {
+            int sell = prices[index] + solve(prices, index+1, 1, limit - 1, dp);
+            int skip = 0 + solve(prices, index+1, 0, limit, dp);
+            profit = max(sell, skip);
+        }
+        return dp[index][buy][limit] = profit;
+    }
+
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        return solve(prices, 0, 1, k, dp);
+    }
+};
