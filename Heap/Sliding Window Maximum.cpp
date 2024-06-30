926dfc46a9ad6b282448389b47701cfd3764300c
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-06-30 10:17:08 +0530

    Create Sliding Window Maximum.cpp

---
+++ Heap/Sliding Window Maximum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>>pq;
+        vector<int>ans;
+        for(int i = 0; i < k; i++) 
+            pq.push({nums[i], i});
+        ans.push_back(pq.top().first);
+        for(int i = k; i < nums.size(); i++) {
+            pq.push({nums[i], i});
+            while(pq.top().second <= i-k)   pq.pop();
+            ans.push_back(pq.top().first);
+        }
+        return ans;
+    }
+};
